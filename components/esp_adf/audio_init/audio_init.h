3f4e49de983381265e9c3e80f142386ee601d712
dwit <141568444+dwitgen@users.noreply.github.com>
2024-12-14 16:14:07 -0500

    Update audio_init.h re-create

--- components/esp_adf/audio_init/audio_init.h
+++ components/esp_adf/audio_init/audio_init.h
@@ -5,9 +5,11 @@
 #include "audio_element.h"
 #include "esp_err.h"
 
-// Function prototypes
-esp_err_t audio_init_pipeline(audio_pipeline_handle_t *pipeline);
-esp_err_t audio_init_i2s_writer(audio_element_handle_t *i2s_writer);
-esp_err_t audio_init_resample_filter(audio_element_handle_t *filter);
+class AudioInit {
+public:
+    esp_err_t configure_i2s_stream_writer_http(audio_element_handle_t *i2s_stream_writer);
+    esp_err_t configure_i2s_stream_writer_raw(audio_element_handle_t *i2s_stream_writer);
+    esp_err_t configure_resample_filter(audio_element_handle_t *filter);
+};
 
 #endif // AUDIO_INIT_H
