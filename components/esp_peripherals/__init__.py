8460baaf58c08814efa8f53e16098a7159b0f331
dwit <141568444+dwitgen@users.noreply.github.com>
2025-01-23 13:04:22 -0500

    Update __init__.py

--- components/esp_peripherals/__init__.py
+++ components/esp_peripherals/__init__.py
@@ -2,9 +2,6 @@ import esphome.codegen as cg
 import esphome.config_validation as cv
 from esphome.const import CONF_ID
 
-CODEOWNERS = ["@your-github-username"]  # Replace with your GitHub username
-DEPENDENCIES = ["esp32"]  # If it depends on other components
-
 esp_peripherals_ns = cg.esphome_ns.namespace("esp_peripherals")
 ESPPeripherals = esp_peripherals_ns.class_("ESPPeripherals", cg.Component)
 
