f9735e108dab8808a41867bfc01b598bdd563730
dwit <141568444+dwitgen@users.noreply.github.com>
2025-01-24 13:48:19 -0500

    Update esp_adf_speaker.cpp

--- components/esp_adf/speaker/esp_adf_speaker.cpp
+++ components/esp_adf/speaker/esp_adf_speaker.cpp
@@ -5,10 +5,21 @@
 #include <driver/i2s.h>
 // Added includes for button controls
 #include <driver/gpio.h>
-#include <driver/adc.h>
-//#include "<esp_adc_cal_internal_legacy.h>"
+//#include <driver/adc.h>
+//#include <esp_adc_cal.h>
+
+#ifdef USE_ESP32
+#if ESP_IDF_VERSION >= ESP_IDF_VERSION_VAL(5, 0, 0)
+#include "hal/adc_types.h"  // This defines ADC_CHANNEL_MAX
+#include "esp_adc/adc_oneshot.h"
+#include "esp_adc/adc_cali.h"
+#include "esp_adc/adc_cali_scheme.h"
+#include "driver/adc_types_legacy.h"
+#else
 #include <esp_adc_cal.h>
-#error "Testing whether this file is being compiled"
+#include "driver/adc.h"
+#endif  // ESP_IDF_VERSION >= ESP_IDF_VERSION_VAL(5, 0, 0)
+#endif  // USE_ESP32
 
 #include "esphome/core/application.h"
 #include "esphome/core/hal.h"
