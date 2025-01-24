941ae6bca2de56b1a02423bad443a866176cb6d9
dwit <141568444+dwitgen@users.noreply.github.com>
2025-01-24 09:17:25 -0500

    Update esp_adf_speaker.h Adding new includes for buttons
    Added: 
    #include "driver/gpio.h"
    #include "driver/adc.h"
    #include "esp_adc_cal.h" 
    This an attempt to move the buttons from esp-adf to esp-idf as the esp-adf uses esp_adc_cal which is depricated in esp-idf.

--- components/esp_adf/speaker/esp_adf_speaker.h
+++ components/esp_adf/speaker/esp_adf_speaker.h
@@ -17,11 +17,15 @@
 #include <audio_pipeline.h>
 
 #include <audio_hal.h>
-#include "esp_peripherals.h"
-#include "periph_adc_button.h"
-#include "input_key_service.h"
+//#include "esp_peripherals.h"
+//#include "periph_adc_button.h"
+//#include "input_key_service.h"
 #include <board.h>
 #include <http_stream.h>
+
+#include "driver/gpio.h"
+#include "driver/adc.h"
+#include "esp_adc_cal.h"
 
 #include <esp_event.h>  
 
