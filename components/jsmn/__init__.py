04a6b6cc3fce384512d0fd1891f3c9832d641903
dwit <141568444+dwitgen@users.noreply.github.com>
2025-01-24 19:33:20 -0500

    Update __init__.py

--- components/jsmn/__init__.py
+++ components/jsmn/__init__.py
@@ -1,7 +1,27 @@
-CODEOWNERS = ["@your_github_username"]
+import esphome.codegen as cg
+import esphome.config_validation as cv
+from esphome.components import esp32
+from esphome.const import CONF_ID
 
-def setup_platform(config):
-    pass
+CODEOWNERS = [""]
+DEPENDENCIES = ["esp32"]
 
-def setup_component(config):
-    pass
+jsmn_ns = cg.esphome_ns.namespace("jsmn")
+JSMNComponent = jsmn_ns.class_("JSMNComponent", cg.Component)
+
+CONFIG_SCHEMA = cv.Schema(
+    {
+        cv.GenerateID(): cv.declare_id(JSMNComponent),
+    }
+).extend(cv.COMPONENT_SCHEMA)
+
+async def to_code(config):
+    var = cg.new_Pvariable(config[CONF_ID])
+    await cg.register_component(var, config)
+
+    esp32.add_idf_component(
+        name="jsmn",
+        repo="https://github.com/dwitgen/jsmn",
+        path=".",
+        components=["*"],
+    )
