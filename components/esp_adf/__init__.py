ba4f062f63e1130be728eb9b3ff9793ddf9395fb
dwit <141568444+dwitgen@users.noreply.github.com>
2025-01-29 11:48:43 -0500

    Update __init__.py

--- components/esp_adf/__init__.py
+++ components/esp_adf/__init__.py
@@ -81,7 +81,7 @@ async def to_code(config):
         name="esp-adf",
         repo="https://github.com/espressif/esp-adf",
         path="components",
-        ref="v2.7",
+        ref="master",
         components=["*"],
         submodules=["components/esp-sr", "components/esp-adf-libs"],
     )
