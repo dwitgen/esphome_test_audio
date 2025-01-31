e1442d5ca6d5eda96b39c46a0a035d18c3845af0
dwit <141568444+dwitgen@users.noreply.github.com>
2025-01-31 14:08:06 -0500

    Update esp_adf_speaker.cpp

--- components/esp_adf/speaker/esp_adf_speaker.cpp
+++ components/esp_adf/speaker/esp_adf_speaker.cpp
@@ -75,7 +75,7 @@ esp_err_t ESPADFSpeaker::configure_i2s_stream(audio_element_handle_t *i2s_stream
         .multi_out_num = 0,                        // Single output
         .uninstall_drv = true,                     // Uninstall driver on destruction
         .need_expand = false,                      // No data expansion needed
-        .expand_src_bits = I2S_DATA_BIT_WIDTH_16BIT, // Source bit width
+        //.expand_src_bits = I2S_DATA_BIT_WIDTH_16BIT, // Source bit width
         .buffer_len = I2S_STREAM_BUF_SIZE,
     };
     
