3e73ca57f17f861fbc959432c09195d00040f93a
dwit <dwitgen+github@gmail.com>
2025-02-07 15:20:51 -0500

    Testing adc buttons in speaker directly

--- components/esp_adf/speaker/esp_adf_speaker.cpp
+++ components/esp_adf/speaker/esp_adf_speaker.cpp
@@ -45,10 +45,10 @@ void ESPADFSpeaker::setup_adc() {
         .unit_id = ADC_UNIT_1,
         .ulp_mode = ADC_ULP_MODE_DISABLE,  // Ensure ULP mode is disabled
     };
-    while (true) {
-        ESP_LOGI(TAG, "✅ Hanging here AFTER initialize_adc_calibration()");
-        vTaskDelay(pdMS_TO_TICKS(1000));
-    }
+    //while (true) {
+    //    ESP_LOGI(TAG, "✅ Hanging here AFTER initialize_adc_calibration()");
+    //    vTaskDelay(pdMS_TO_TICKS(1000));
+    //}
     ESP_ERROR_CHECK(adc_oneshot_new_unit(&init_config, &this->adc1_handle));
 
     ESP_LOGI(TAG, "ADC Unit Initialized");
@@ -59,10 +59,10 @@ void ESPADFSpeaker::setup_adc() {
         .bitwidth = ADC_BITWIDTH_DEFAULT       // Use default bit width
     };
     ESP_ERROR_CHECK(adc_oneshot_config_channel(adc1_handle, ADC_CHANNEL_7, &chan_config));  // GPIO8 maps to ADC_CHANNEL_7
-    //while (true) {
-    //    ESP_LOGI(TAG, "✅ Hanging here AFTER initialize_adc_calibration()");
-    //    vTaskDelay(pdMS_TO_TICKS(1000));
-    //}
+    while (true) {
+        ESP_LOGI(TAG, "✅ Hanging here AFTER initialize_adc_calibration()");
+        vTaskDelay(pdMS_TO_TICKS(1000));
+    }
     // Step 3: ADC Calibration (Optional but safe)
     //adc_calibrated = setup_adc_calibration(ADC_UNIT_1, ADC_CHANNEL_7, ADC_ATTEN_DB_12, &adc1_cali_handle);
 
