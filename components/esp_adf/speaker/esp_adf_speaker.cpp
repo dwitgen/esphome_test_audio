96fca7e8a96b47ef95e5c5032ac841df202cb964
dwit <dwitgen+github@gmail.com>
2025-02-07 15:54:49 -0500

    Testing adc buttons in speaker directly

--- components/esp_adf/speaker/esp_adf_speaker.cpp
+++ components/esp_adf/speaker/esp_adf_speaker.cpp
@@ -58,7 +58,7 @@ void ESPADFSpeaker::setup_adc() {
         .atten = ADC_ATTEN_DB_12,              // 12dB attenuation
         .bitwidth = ADC_BITWIDTH_DEFAULT       // Use default bit width
     };
-    ESP_ERROR_CHECK(adc_oneshot_config_channel(this->adc1_handle, ADC1_CHANNEL_7, &chan_config));  // GPIO8 maps to ADC_CHANNEL_7
+    ESP_ERROR_CHECK(adc_oneshot_config_channel(this->adc1_handle, ADC_CHANNEL_7, &chan_config));  // GPIO8 maps to ADC_CHANNEL_7
     while (true) {
         ESP_LOGE(TAG, "✅ Hanging here AFTER initialize_adc_calibration()");
         vTaskDelay(pdMS_TO_TICKS(1000));
