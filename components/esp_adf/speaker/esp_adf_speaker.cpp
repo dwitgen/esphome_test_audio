bc28e69af67c7e7796fde046e7ffd962daa8a578
dwit <dwitgen+github@gmail.com>
2025-02-07 20:58:16 -0500

    Testing adc buttons in speaker directly

--- components/esp_adf/speaker/esp_adf_speaker.cpp
+++ components/esp_adf/speaker/esp_adf_speaker.cpp
@@ -504,14 +504,14 @@ void ESPADFSpeaker::setup() {
   adc_oneshot_unit_init_cfg_t init_config1 = {
       .unit_id = ADC_UNIT_1,
   };
-  ESP_ERROR_CHECK(adc_oneshot_new_unit(&init_config1, &adc1_handle));
+  ESP_ERROR_CHECK(adc_oneshot_new_unit(&init_config1, &this->adc1_handle));
   adc_oneshot_chan_cfg_t ch_config = {
       .atten = ADC_ATTEN_DB_12,
       .bitwidth = ADC_BITWIDTH_12,
   };
-  ESP_ERROR_CHECK(adc_oneshot_config_channel(&adc1_handle, ADC_CHANNEL_7, &ch_config));
+  ESP_ERROR_CHECK(adc_oneshot_config_channel(&this->adc1_handle, ADC_CHANNEL_7, &ch_config));
   adc_cali_handle_t adc1_cali_handle = NULL;
-  bool adc_calibrated = setup_adc_calibration(ADC_UNIT_1, ADC_CHANNEL_7, ADC_ATTEN_DB_12, &adc1_cali_handle);
+  bool adc_calibrated = setup_adc_calibration(ADC_UNIT_1, ADC_CHANNEL_7, ADC_ATTEN_DB_12, &this->adc1_cali_handle);
  // adc1_config_width(ADC_WIDTH_BIT);
  // adc1_config_channel_atten((adc1_channel_t)but_channel, ADC_ATTEN);
 
