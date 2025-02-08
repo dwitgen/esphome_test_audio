4b47ba9aa9bc86b4f11ae5c39c4ab5c3c2ac3afd
dwit <dwitgen+github@gmail.com>
2025-02-08 16:35:49 -0500

    Testing adc buttons in speaker directly

--- components/esp_adf/speaker/esp_adf_speaker.h
+++ components/esp_adf/speaker/esp_adf_speaker.h
@@ -59,12 +59,12 @@ class ESPADFSpeaker : public ESPADFPipeline, public speaker::Speaker, public Com
   //void update_playback_state(int state);
   void update_playback_state(const char *state);
 
-  binary_sensor::BinarySensor *btn_vol_up;
-  binary_sensor::BinarySensor *btn_vol_down;
-  binary_sensor::BinarySensor *btn_set;
-  binary_sensor::BinarySensor *btn_play;
-  binary_sensor::BinarySensor *btn_mode;
-  binary_sensor::BinarySensor *btn_record;
+  binary_sensor::BinarySensor *internal_btn_vol_up;
+  binary_sensor::BinarySensor *internal_btn_vol_down;
+  binary_sensor::BinarySensor *internal_btn_set;
+  binary_sensor::BinarySensor *internal_btn_play;
+  binary_sensor::BinarySensor *internal_btn_mode;
+  binary_sensor::BinarySensor *internal_btn_record;
 
   static esp_err_t input_key_service_cb(periph_service_handle_t handle, periph_service_event_t *evt, void *ctx);
   void handle_set_button();
