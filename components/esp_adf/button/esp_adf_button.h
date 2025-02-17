40b48c6206ed060644aeb7297c6350efc09d703f
dwit <dwitgen+github@gmail.com>
2025-02-17 09:02:03 -0500

    Working on using esp_peripherals in speaker

--- components/esp_adf/button/esp_adf_button.h
+++ components/esp_adf/button/esp_adf_button.h
@@ -4,6 +4,7 @@
 #include "esp_peripherals.h"
 #include "periph_adc_button.h"
 #include "input_key_service.h"
+#include "esp_adf_speaker.h"
 
 namespace esphome {
 namespace esp_adf {
@@ -12,6 +13,11 @@ class ESPADFButton : public Component {
  public:
   void setup() override;
   static esp_err_t input_key_service_cb(periph_service_handle_t handle, periph_service_event_t *evt, void *ctx);
+
+  void set_speaker(ESPADFSpeaker *speaker) { this->speaker_ = speaker; }  // ✅ Store speaker reference
+
+ private:
+  ESPADFSpeaker *speaker_ = nullptr; 
 };
 
 }  // namespace esp_adf
