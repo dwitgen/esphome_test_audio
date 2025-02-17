774d7573cd224642c5ef997548f06d7c432712ec
dwit <dwitgen+github@gmail.com>
2025-02-17 10:03:18 -0500

    Working on using esp_peripherals in speaker

--- components/esp_adf/button/__init__.py
+++ components/esp_adf/button/__init__.py
@@ -32,3 +32,4 @@ async def to_code(config):
     var = cg.new_Pvariable(config[CONF_ID])
     await cg.register_component(var, config)
     await cg.register_parented(var, config[CONF_ESP_ADF_ID])
+    await button.register_button(var, config)
