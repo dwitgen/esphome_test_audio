5d5e1e91f0839487c165b2fc614dbd3ff38dc4e7
dwit <dwitgen+github@gmail.com>
2025-02-17 12:48:58 -0500

    Working on using esp_peripherals in speaker

--- components/esp_adf/button/__init__.py
+++ components/esp_adf/button/__init__.py
@@ -1,7 +1,7 @@
 import esphome.codegen as cg
 import esphome.config_validation as cv
 from esphome.components import button
-from esphome.const import CONF_ID, CONF_NAME, CONF_DISABLED_BY_DEFAULT
+from esphome.const import CONF_ID #, CONF_NAME, CONF_DISABLED_BY_DEFAULT
 
 from .. import (
     CONF_ESP_ADF_ID,
@@ -14,15 +14,15 @@ from .. import (
 AUTO_LOAD = ["esp_adf"]
 DEPENDENCIES = ["esp32"]
 
-ESPADFButton = esp_adf_ns.class_("ESPADFButton", ESPADFPipeline, button.Button, cg.Component)
+ESPADFButton = esp_adf_ns.class_("ESPADFButton",  cg.Component)
 
 CONFIG_SCHEMA = cv.All(
     cv.Schema(
         {
             cv.GenerateID(): cv.declare_id(ESPADFButton),
             cv.GenerateID(CONF_ESP_ADF_ID): cv.use_id(ESPADF),
-            cv.Optional(CONF_NAME): cv.string,
-            cv.Optional(CONF_DISABLED_BY_DEFAULT, default=False): cv.boolean,
+           # cv.Optional(CONF_NAME): cv.string,
+           # cv.Optional(CONF_DISABLED_BY_DEFAULT, default=False): cv.boolean,
         }
     ).extend(cv.COMPONENT_SCHEMA),
     cv.only_with_esp_idf,
@@ -34,7 +34,7 @@ FINAL_VALIDATE_SCHEMA = final_validate_usable_board("button")
 async def to_code(config):
     var = cg.new_Pvariable(config[CONF_ID])
     await cg.register_component(var, config)
-    await cg.register_parented(var, config[CONF_ESP_ADF_ID])
+    #await cg.register_parented(var, config[CONF_ESP_ADF_ID])
 
-    await button.register_button(var, config)
+    #await button.register_button(var, config)
     
