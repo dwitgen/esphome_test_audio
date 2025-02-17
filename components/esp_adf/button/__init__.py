ccf3bf05025ef1485f062798e84c8f63caba2b8a
dwit <dwitgen+github@gmail.com>
2025-02-17 13:42:03 -0500

    Working on using esp_peripherals in speaker

--- components/esp_adf/button/__init__.py
+++ components/esp_adf/button/__init__.py
@@ -1,7 +1,7 @@
 import esphome.codegen as cg
 import esphome.config_validation as cv
 from esphome.components import button
-from esphome.const import CONF_ID #, CONF_NAME, CONF_DISABLED_BY_DEFAULT
+from esphome.const import CONF_ID , CONF_NAME, CONF_DISABLED_BY_DEFAULT
 
 from .. import (
     CONF_ESP_ADF_ID,
@@ -21,8 +21,8 @@ CONFIG_SCHEMA = cv.All(
         {
             cv.GenerateID(): cv.declare_id(ESPADFButton),
             cv.GenerateID(CONF_ESP_ADF_ID): cv.use_id(ESPADF),
-           # cv.Optional(CONF_NAME): cv.string,
-           # cv.Optional(CONF_DISABLED_BY_DEFAULT, default=False): cv.boolean,
+            cv.Optional(CONF_NAME): cv.string,
+            cv.Optional(CONF_DISABLED_BY_DEFAULT, default=False): cv.boolean,
         }
     ).extend(cv.COMPONENT_SCHEMA),
     cv.only_with_esp_idf,
@@ -36,5 +36,5 @@ async def to_code(config):
     await cg.register_component(var, config)
     #await cg.register_parented(var, config[CONF_ESP_ADF_ID])
 
-    #await button.register_button(var, config)
+    await button.register_button(var, config)
     
