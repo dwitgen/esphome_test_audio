15ecb805a11e0c3d96688d818974b03951cae859
dwit <dwitgen+github@gmail.com>
2025-02-21 12:38:38 -0500

    Working on using esp_peripherals in speaker

--- components/esp_adf/button/__init__.py
+++ components/esp_adf/button/__init__.py
@@ -1,6 +1,6 @@
 import esphome.codegen as cg
 import esphome.config_validation as cv
-from esphome.components import binary_sensor # button
+from esphome.components import binary_sensor, button
 from esphome.const import CONF_ID
 
 from .. import (
