e8d98db2bea4cd5d5f412080afd7a6eb5bbce770
dwit <dwitgen+github@gmail.com>
2025-02-23 15:35:43 -0500

    Working on using esp_peripherals in speaker

--- components/esp_adf/button/__init__.py
+++ components/esp_adf/button/__init__.py
@@ -125,11 +125,11 @@ async def to_code(config):
         #        cg.add(sensor.add_on_press(await automation.build_automation(
         #            sensor.get_press_trigger(), [], sensor_config[CONF_ON_PRESS]
         #        )))
-        if CONF_ON_PRESS in sensor_config[button_id]:
+        if CONF_ON_PRESS in config[button_id]:
                 trigger = await automation.build_automation(
                     sensor_obj.get_press_trigger(),
                     [],
-                    sensor_config[button_id][CONF_ON_PRESS],
+                    config[button_id][CONF_ON_PRESS],
                 )
                 cg.add(sensor_obj.add_on_press(trigger))
 
