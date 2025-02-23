ae2c2da9dcdbc13d922af849764a856ac3bb3155
dwit <dwitgen+github@gmail.com>
2025-02-23 09:54:44 -0500

    Working on using esp_peripherals in speaker

--- components/esp_adf/button/__init__.py
+++ components/esp_adf/button/__init__.py
@@ -104,21 +104,21 @@ async def to_code(config):
         cg.add(getattr(var, f"set_{button_id}")(sensor_obj))
         
         # Handle actions (on_press / on_release)
-        if "on_press" in sensor_config:
-            await automation.build_automation(
-                sensor_obj, sensor_config["on_press"], cg.RawExpression("state")
-            )
-
-        if "on_release" in sensor_config:
-            await automation.build_automation(
-                sensor_obj, sensor_config["on_release"], cg.RawExpression("!state")
-            )
         #if "on_press" in sensor_config:
-        #        await automation.build_automation(
-        #            sensor_obj.add_on_state_callback, sensor_config["on_press"]
-        #        )
+        #    await automation.build_automation(
+        #        sensor_obj, sensor_config["on_press"], cg.RawExpression("state")
+        #    )
 
         #if "on_release" in sensor_config:
         #    await automation.build_automation(
-        #        sensor_obj.add_on_state_callback, sensor_config["on_release"]
+        #        sensor_obj, sensor_config["on_release"], cg.RawExpression("!state")
         #    )
+        if "on_press" in sensor_config:
+                await automation.build_automation(
+                    sensor_obj.add_on_state_callback, sensor_config["on_press"]
+                )
+
+        if "on_release" in sensor_config:
+            await automation.build_automation(
+                sensor_obj.add_on_state_callback, sensor_config["on_release"]
+            )
